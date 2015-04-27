128adf365c66e1a621da8fb993e7b5c01f67f56c
klel <klimelm@gmail.com>
2015-04-27 07:39:43 +0500

    Model for main

---
+++ kimts/Models/jsTree/JSONalternativeFormat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Extensions.Models.jsTree
+{
+    public class JSONalternativeFormat
+    {
+        public string id { get; set; }
+        public string parent { get; set; }
+        public string text { get; set; }
+        public string icon { get; set; }
+        public State state { get; set; }
+        public bool children { get { return false; } private set { ;} }
+    }
+
+    public  class State
+    {
+       bool opened { get; set;}
+       bool disabled { get; set;}
+       bool selected { get; set; }
+ 
+    }
+}
